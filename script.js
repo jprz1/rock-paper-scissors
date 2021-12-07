f9762d13974e1486ac25be2ce67d79f4c1a6d0fc
Jon <jonatanpereztx@gmail.com>
2021-12-07 00:41:13 -0800

    Update script.js with code for rock, paper, scissors

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
 function computerPlay() {
-    let moves = ["Rock", "Paper", "Scissors"]
-    console.log(moves[(Math.floor(Math.random() * moves.length))])
+    let moves = ["rock", "paper", "scissors"];
+
+    return(moves[(Math.floor(Math.random() * moves.length))])
+}
+
+function playRound(player, computerSelection) {
+    let playerSelection = player.toLowerCase();
+
+    //both select same 
+    if (computerSelection === playerSelection) {
+        return `You both selected ${computerSelection}! Please try again.`
+    
+    } else {
+        switch (true) {
+            case (computerSelection === "rock" && playerSelection === "scissors") :
+                return "You Lose! Rock beats Scissors";
+            case (computerSelection === "rock" && playerSelection === "paper"):
+                return "You Win! Paper beats Rock";
+            case (computerSelection === "paper" && playerSelection === "scissors"):
+                return "You Win! Scissors beats Paper";
+            case(computerSelection === "paper" && playerSelection === "rock"):
+                return "You Lose! Paper beats Rock";
+            case(computerSelection === "scissors" && playerSelection === "rock"):
+                return "You Win! Rock beats Scissors";
+            case(computerSelection === "scissors" && playerSelection === "paper"):
+                return "You Lose! Scissors beats Paper";
+
+        } 
+    }
+}
+
+function game() {
+    let selection = window.prompt("Press 'n' to exit. Otherwise input 'Paper', 'Scissors', or 'Rock' to play!")
+
+    while (selection !== 'n') {
+        console.log(playRound(selection, computerPlay()));
+        selection = window.prompt("Press 'n' to exit. Otherwise input 'Paper', 'Scissors', or 'Rock' to play!")
+    }
+    console.log("Thank you for playing!")
 }
